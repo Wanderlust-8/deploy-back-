de95c0ecf76cb9bd602604fd0b16ebe6da3b78b6
Wanderlust-8 <wanderlusthenry8@gmail.com>
2023-07-21 19:08:10 +0200

    modularizacion de index y app.js

--- back/index.js
+++ back/index.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const server = express();
-const routes = require("./src/routes/index.routes");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require('morgan');
+// const express = require("express");
+const server = require('./src/app.js');
+// const routes = require("./src/routes/index.routes");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const morgan = require('morgan');
 const { conn } = require("./src/database");
 require('dotenv').config();
-require('./src/database.js');
+// require('./src/database.js');
 
-server.name = 'BACK';
+// server.name = 'BACK';
 
-//midleweares
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
-server.use(morgan('dev'));
+// //midleweares
+// server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// server.use(bodyParser.json({ limit: "50mb" }));
+// server.use(cookieParser());
+// server.use(morgan('dev'));
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+// server.use(express.json());
+// server.use(express.urlencoded({ extended: false }));
 
-//Routes
-server.use("/", routes);
+// //Routes
+// server.use("/", routes);
 
-//agregue esto
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
+// //agregue esto
+// server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+//   const status = err.status || 500;
+//   const message = err.message || err;
+//   console.error(err);
+//   res.status(status).send(message);
+// });
 
 const PORT = process.env.PORT || 3002;
 
