1ad2af515d80343d8a1860ee31a7c927d08c0654
Wanderlust-8 <wanderlusthenry8@gmail.com>
2023-07-21 18:52:23 +0200

    agregue codigo de error en index.js

--- back/index.js
+++ back/index.js
@@ -32,6 +32,14 @@ server.use(express.urlencoded({ extended: false }));
 //Routes
 server.use("/", routes);
 
+//agregue esto
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
 const PORT = process.env.PORT || 3002;
 
 // Syncing all the models at once.
