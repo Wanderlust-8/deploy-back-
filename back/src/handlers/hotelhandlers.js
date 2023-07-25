1492ba3e187196f88fe9827fa1d71321c7268a0f
Solsantacruz <solsantacruz69@gmail.com>
2023-07-25 17:44:49 +0200

    que funcione

--- back/src/handlers/hotelhandlers.js
+++ back/src/handlers/hotelhandlers.js
@@ -17,7 +17,7 @@ router.get('/' , async (req, res) => {
    try {
     const resultado = name ? await searchNameHotel(name) : await getHotel();
     resultado.length ?
-        res.status(200).json(resultado) : res.status(404).json({error: error.message})
+        res.status(200).json(resultado) : res.status(404).json("No hay hoteles")
    } catch (error) {
     res.status(500).json({ message: error.message });
    }
